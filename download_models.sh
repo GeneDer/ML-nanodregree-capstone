db1fa7fb1160bd542aeb31839e7ffb4cdb6bd453
Gene Der Su <e870252314@yahoo.com.tw>
2016-10-18 12:25:57 -0700

    feat: added model download script
    This script will download the pertained check point files and the
    googlenet graph into the correct directory

---
+++ download_models.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $DIR
+
+echo "Downloading..."
+
+cd data
+wget http://russellsstewart.com/s/tensorbox/googlenet.pb
+wget https://www.dropbox.com/s/qyy7xw5zrk9o009/classification_model.ckpt?dl=1
+wget https://www.dropbox.com/s/w7dvylkgpzri8ca/overfeat_checkpint.ckpt?dl=1
+
+echo "Done."
